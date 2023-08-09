e222b5acf340c60e7690629829b548b903409e81
Ljuzifer <121060706+Ljuzifer@users.noreply.github.com>
2023-08-09 22:27:25 +0300

    start

---
+++ src/components/FeedbackButtons/FeedbackButtons.jsx
@@ -0,0 +1,18 @@
+export const FeedbackButtons = () => {
+  return (
+    <div>
+      <h2>Feedback</h2>
+      <ul>
+        <li>
+          <button type="button">good</button>
+        </li>
+        <li>
+          <button type="button">neutral</button>
+        </li>
+        <li>
+          <button type="button">bad</button>
+        </li>
+      </ul>
+    </div>
+  );
+};
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,24 @@
+export const Statistics = () => {
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <ul>
+        <li>
+          <p>item</p>
+        </li>
+        <li>
+          <p>item</p>
+        </li>
+        <li>
+          <p>item</p>
+        </li>
+        <li>
+          <p>item</p>
+        </li>
+        <li>
+          <p>item</p>
+        </li>
+      </ul>
+    </div>
+  );
+};
